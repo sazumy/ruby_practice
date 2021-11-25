5831da50e0983c12cc599e34f35f744510d5ef5d
sazumy <49363119+sazumy@users.noreply.github.com>
2021-11-25 23:27:11 +0900

    エラーの補足方法の様々なバリエーション

--- chapter/09_error/error_object.rb
+++ chapter/09_error/error_object.rb
@@ -20,4 +20,13 @@ begin
   'abc'.foo
 rescue ZeroDivisionError
   puts "0で除算しました"
+rescue NoMethodError
+  puts "存在しないメソッドが呼び出されました"
+end
+
+begin
+  'abc'.foo
+rescue ZeroDivisionError, NoMethodError => e
+  puts "0で除算したか、存在しないメソッドが呼び出されました"
+  puts "エラー：#{e.class} #{e.message}"
 end
