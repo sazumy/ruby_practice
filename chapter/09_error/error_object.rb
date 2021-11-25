30c38a9962c6df7a931703d81785ecd86101aae9
sazumy <49363119+sazumy@users.noreply.github.com>
2021-11-25 23:23:36 +0900

    補足するエラーオブジェクトを限定する例

--- chapter/09_error/error_object.rb
+++ chapter/09_error/error_object.rb
@@ -1,3 +1,4 @@
+# エラーオプジェクトの中身を確認する例
 begin
   1 / 0
 rescue => e
@@ -6,4 +7,17 @@ rescue => e
   puts "バックトレース ------------"
   puts e.backtrace
   puts "-------------------------"
+end
+
+# 指定したエラー名のエラーしか捕捉しない例
+begin
+  1 / 0
+rescue ZeroDivisionError
+  puts "0で除算しました"
+end
+
+begin
+  'abc'.foo
+rescue ZeroDivisionError
+  puts "0で除算しました"
 end
